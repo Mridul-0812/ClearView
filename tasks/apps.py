d70908ba8e6470e06fb7a9345cd11c01ae505a93
Mridul-0812 <guptamridul2009@gmail.com>
2026-04-03 13:23:28 +1100

    Apps.py modified

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_admin(sender, **kwargs):
+    from django.contrib.auth.models import User
+    # This creates a fresh account on Render's Postgres database
+    if not User.objects.filter(username='Mridul-Render').exists():
+        User.objects.create_superuser('Mridul-Render', 'your-email@example.com', 'YourPassword123!')
 
 class TasksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+
+    def ready(self):
+        post_migrate.connect(create_admin, sender=self)
